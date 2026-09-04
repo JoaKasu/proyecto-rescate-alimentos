5a4ba6c314fc37ba35bfaab6dc46c4f39f6c276c
JoaKasu <joacoqo3@gmail.com>
2026-09-03 22:57:35 -0400

    Entrega proyecto base Django funcional v3

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Alimento
 
-admin.site.register(Alimento)
+class AlimentoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'categoria', 'cantidad', 'disponible', 'fecha_limite']
+
+admin.site.register(Alimento, AlimentoAdmin)
